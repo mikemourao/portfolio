04b9d087feb8c94df3a6baf903ead2ea40f94c3d
Mike Igor Mourão da Silva <mike.silva@grupoicts.com.br>
2023-08-25 15:06:19 -0400

    feat: start of about experience

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,9 +1,18 @@
-import { Button, Col, Image, Layout, Row, Typography } from "antd";
+import { Button, Card, Col, Image, Layout, Row, Typography } from "antd";
 import logo from '../../assets/Logo.png';
+import smd from '../../assets/smd.png';
+import dash from '../../assets/dash.png';
 import user from '../../assets/Me.svg';
 import './styles.css';
 
 export function App() {
+    const scrollToAbout = () => {
+        const aboutSection = document.getElementById('about-section');
+        if (aboutSection) {
+            aboutSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <Layout className="portfolio-layout-aling">
             <Row className="portfolio-header-styles" justify="center" align="middle">
@@ -34,6 +43,7 @@ export function App() {
                             fontWeight: "bold",
                             fontSize: 18
                         }}
+                        onClick={scrollToAbout}
                     >
                         About
                     </Button>
@@ -81,6 +91,73 @@ export function App() {
                         de Manaus, e estou constantemente em busca de novos conhecimentos e desafios para <br></br>
                         aprimorar minha atuação.</p>}
                 </Typography.Title>
+            </Row>
+            {/* about */}
+            <Row id="about-section" className="portfolio-body-styles">
+                <Typography.Title
+                    level={1}
+                    style={{ color: "#FFFF" }}
+                >
+                    Experiência de Trabalho
+                </Typography.Title>
+            </Row>
+            <Row className="portfolio-body-styles">
+                <Row gutter={30}>
+                    <Col span={12}>
+                        <Card bordered={false} className="portfolio-card-styles">
+                            <Row>
+                                <Col span={12}>
+                                    <Image
+                                        style={{ padding: 15 }}
+                                        src={smd}
+                                        alt="Portfolio Mike"
+                                    />
+                                </Col>
+                                <Col span={12}>
+                                    <Typography.Title
+                                        level={3}
+                                        style={{ color: "#FFFF" }}
+                                    >
+                                        Experiência de Trabalho
+                                    </Typography.Title>
+                                    <Typography.Title
+                                        italic
+                                        style={{ color: "#FFFF", fontSize: 12 }}
+                                    >
+                                        Descrição da experiência de trabalho
+                                    </Typography.Title>
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+                    <Col span={12}>
+                        <Card bordered={false} className="portfolio-card-styles">
+                            <Row>
+                                <Col span={12}>
+                                    <Image
+                                        style={{ padding: 15 }}
+                                        src={dash}
+                                        alt="Portfolio Mike"
+                                    />
+                                </Col>
+                                <Col span={12}>
+                                    <Typography.Title
+                                        level={3}
+                                        style={{ color: "#FFFF" }}
+                                    >
+                                        Experiência de Trabalho
+                                    </Typography.Title>
+                                    <Typography.Title
+                                        italic
+                                        style={{ color: "#FFFF", fontSize: 12 }}
+                                    >
+                                        Descrição da experiência de trabalho
+                                    </Typography.Title>
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+                </Row>
             </Row>
         </Layout>
     );
