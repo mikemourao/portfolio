232e2a79f7f148379268da42f47477cec37f8cd9
Mike Igor Mourão da Silva <mike.silva@grupoicts.com.br>
2023-08-25 11:12:39 -0400

    feat: initial header

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,10 +1,24 @@
-import { Layout } from "antd";
-import './styles.css'
+import { Button, Col, Image, Layout, Row } from "antd";
+import logo from '../../assets/Logo.png';
+import './styles.css';
 
 export function App() {
     return (
         <Layout className="portfolio-layout-aling">
-
+            <Row className="portfolio-header-styles" justify="center" align="middle"> {/* Adicione align="middle" para alinhar ao centro verticalmente */}
+                <Col span={12}>
+                    <Image
+                        style={{ padding: 5 }}
+                        src={logo}
+                        alt="Portfolio Mike"
+                    />
+                </Col>
+                <Col span={12} style={{ textAlign: 'right' }}>
+                    <Button type="text" style={{ margin: '0 8px', color: "#ffff", fontWeight: "bold"}}>Home</Button>
+                    <Button type="text" style={{ margin: '0 8px', color: "#ffff", fontWeight: "bold"}}>About</Button>
+                    <Button type="text" style={{ margin: '0 8px', color: "#ffff", fontWeight: "bold"}}>Lab</Button>
+                </Col>
+            </Row>
         </Layout>
-    )
+    );
 }
